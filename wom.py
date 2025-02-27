33738a6324a01a94c0b8b8da6fcb58993141c993
Ben Ciani <Benjamin.A.Ciani@gmail.com>
2025-02-26 18:19:00 -0600

    Update wom.py
    adding basic stsructure

--- wom.py
+++ wom.py
@@ -1,13 +1,28 @@
 from prefect import flow, get_run_logger
 from prefect.deployments import Deployment
 from prefect.server.schemas.schedules import CronSchedule
-
+import pandas as pd
 from prefect.infrastructure import DockerContainer
 
 @flow(name="pooSkiller")
 def data_flow():
     logger = get_run_logger()
-    logger.warning("Test Sent")
+    
+    logger.debugg("API Call Start")
+    api_call()
+    logger.debugg("API Call Finished")
+
+@task
+def api_call():
+    logger = get_run_logger()
+
+@task
+def data_sort():
+    logger = get_run_logger()
+
+@task
+def print_results():
+    logger = get_run_logger()
 
 # This is needed to signal the main/start of the job
 if __name__ == "__main__":
@@ -22,7 +37,7 @@ Deployment.build_from_flow(
     tags=["poo"],
     work_pool_name="default-pool",
     work_queue_name="default",
-    #storage=BitBucketRepository.load("bb-repo"), swap to githubsetup
+    #storage=BitBucketRepository.load("bb-repo"), swap to GitHub setup
     infrastructure= DockerContainer.load("standard-img"),
     infra_overrides=dict(env={"PREFECT_LOGGING_LEVEL":"DEBUG"}),
     path="",
